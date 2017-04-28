d4669908fc8773d5d8d2cb5c6543961a6d578316
Jan Kusniar <jkusniar@gmail.com>
2017-04-28 14:03:30 +0200

    test sql fix

--- postgres_test/schema.sql
+++ postgres_test/schema.sql
@@ -1,12 +1,3 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-SET default_with_oids = false;
-
 CREATE TABLE lov_title (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL UNIQUE
